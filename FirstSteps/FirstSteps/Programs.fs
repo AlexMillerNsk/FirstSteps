c799b9cb93e9e8eef4a6cc0807fadf776d2f6df8
AlexMillerNsk <love.me.lnikov@gmail.com>
2022-04-29 15:38:52 +0700

    gitignore added

--- FirstSteps/FirstSteps/Programs.fs
+++ FirstSteps/FirstSteps/Programs.fs
@@ -4,7 +4,7 @@ open System
 open Elmish
 open Elmish.WPF
 
-type Todo = { Id: int; Value: string }
+type Todo = { Id: int; Value: string; ToDoText: string }
 //Messages
 type Msg =
     | SetState of string
@@ -13,6 +13,9 @@ type Msg =
     | Plus
     | Minus
     | SetCount of int
+    | SetToDos of Todo list
+    | SetToDoText of string * int
+    | UpdateToDo of string * int
 //Models
 type Model =
     { Count: int
@@ -28,8 +31,22 @@ let init () =
 //AddThings
 let AddThings m =
     { m with
-        ToDo = { Id = Random().Next(0,999); Value = m.State }:: m.ToDo
+        ToDo = { Id = Random().Next(0,999); Value = m.State; ToDoText = String.Empty }:: m.ToDo
         State = String.Empty}
+
+let SetToDoText (m: Model) value id = 
+    let todos = m.ToDo
+    let maper t = if t.Id = id then { t with ToDoText = value } else t
+    let todos = List.map maper todos
+    { m with ToDo = todos }
+
+let updateToDo (m: Model) value id =
+    let todos= m.ToDo
+    let updater t = if t.Id = id then { t with Value = value } else t
+    let todos = List.map updater todos
+    { m with ToDo = todos }
+
+
 //Update
 let update msg m =
     match msg with
@@ -39,14 +56,23 @@ let update msg m =
     | SetCount k          -> { m with Count = k}, []
     | Plus                -> { m with Count = m.Count+1},[]
     | Minus               -> { m with Count = m.Count-1},[]
+    | SetToDos t          -> { m with ToDo = t }, []
+    | SetToDoText (v, id) -> SetToDoText m v id, []
+    | UpdateToDo (v, id)  -> updateToDo m v id, []
 //Bindings
+let setToDoText a (m, s) =  Msg.SetToDoText (a, s.Id)
+let ToDoBinding() =
+    [ "RemoveTodo"  |> Binding.cmdParam RemoveToDo
+      "Id"          |> Binding.oneWay (fun (_, s) -> s.Id)
+      "Value"       |> Binding.oneWay (fun (_, s) -> s.Value)
+      "ToDoText"    |> Binding.twoWay ((fun (_, s) -> s.ToDoText),setToDoText)
+      "UpdateTodo"  |> Binding.cmdIf ((fun (_, s) -> UpdateToDo(s.ToDoText, s.Id)),(fun (m, s) -> not <| String.IsNullOrEmpty(s.ToDoText)))  
+    ]
+
 let bindings(): Binding<Model, Msg> list =
     [
         "SetState"      |> Binding.twoWay ((fun m -> m.State), SetState)
-        "ToDo"          |> Binding.subModelSeq ((fun m -> m.ToDo), (fun y -> y.Id), (fun () ->
-        [ "RemoveTodo"  |> Binding.cmdParam RemoveToDo
-          "Id"          |> Binding.oneWay (fun (_, s) -> s.Id)
-          "Value"       |> Binding.oneWay (fun (_, s) -> s.Value) ]))
+        "ToDo"          |> Binding.subModelSeq ((fun m -> m.ToDo), (fun y -> y.Id), ToDoBinding)   
         "AddToDo"       |> Binding.cmdIf (AddTodo, (fun m -> not <| String.IsNullOrEmpty(m.State)))
         "SetCount"      |> Binding.twoWay ((fun m -> m.Count), SetCount)
         "Plus"          |> Binding.cmd Plus
